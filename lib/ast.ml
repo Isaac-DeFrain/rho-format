d088a700de91918c8aa985c85300d06bc13d4785
Isaac DeFrain <isaacdefrain@gmail.com>
2021-05-22 10:22:57 -0600

    Format: add name wildcard and refactor wrap_with_cfg

--- lib/ast.ml
+++ lib/ast.ml
@@ -1,4 +1,5 @@
 [@@@warning "-11"]
+[@@@warning "-27"]
 
 (* Abstract syntax tree *)
 
@@ -56,9 +57,7 @@ and proc_pat =
   | IntPat
   | StrPat
   (* wildcard pattern *)
-  | Wildcard
-  (* pattern variables *)
-  | PatVar of string
+  | PWildcard
   (* processes are patterns too *)
   | Proc of proc
   | BundlePat of bool * bool * proc_pat
@@ -83,7 +82,7 @@ and names = Names of name list
 
 and name_pat =
   | QuotePat of proc_pat
-  | NPatVar of string
+  | NWilcdcard
 
 and name_pats = NPats of name_pat list
 
@@ -95,6 +94,7 @@ and wrappers =
   | BundleZero
   | For of string
   | Send of bool * string
+  | WNew of string
 
 (* viewing functions *)
 let std_cfg = {
@@ -115,7 +115,7 @@ let std_cfg = {
 
 open Printf
 
-let rec view_proc ind ?(cfg = std_cfg) = function
+let rec view_proc ind ?(cfg = std_cfg) ?(new_line = false) = function
 | Nil -> String.make ind ' ' ^ "Nil"
 | Bool b -> String.make ind ' ' ^ string_of_bool b
 | Int i -> String.make ind ' ' ^ string_of_int i
@@ -123,12 +123,12 @@ let rec view_proc ind ?(cfg = std_cfg) = function
 | PVar p -> String.make ind ' ' ^ p
 | Brace p -> String.make ind ' ' ^ wrap_with_cfg ind cfg Braces @@ view_proc ind ~cfg p
 | Paren p -> String.make ind ' ' ^ wrap_with_cfg ind cfg Parens @@ view_proc ind ~cfg p
-| New ((h, tl), p) -> String.make ind ' ' ^ "new " ^ view_names ind cfg (Names (h :: tl)) ^ "{" ^ view_proc (ind + cfg.indent) ~cfg p ^ "}"
+| New ((h, tl), p) -> wrap_with_cfg ind cfg (WNew (view_names ind cfg @@ Names (h :: tl))) @@ view_proc (ind + cfg.indent) ~cfg p
 | Bundle (true,  false, p) -> wrap_with_cfg ind cfg BundlePlus  @@ view_proc ind ~cfg p
 | Bundle (false, true,  p) -> wrap_with_cfg ind cfg BundleMinus @@ view_proc ind ~cfg p
 | Bundle (false, false, p) -> wrap_with_cfg ind cfg BundleZero  @@ view_proc ind ~cfg p
 | Bundle (true,  true,  p) -> view_proc ind ~cfg p
-| Receive (bs, p) -> wrap_with_cfg ind cfg (For (view_binds ind cfg bs)) @@ view_proc (ind + cfg.indent) ~cfg p
+| Receive (bs, p) -> wrap_with_cfg ind cfg (For (view_binds ind cfg bs)) @@ view_proc ind ~cfg p
 | Bang (per, ch, ps) -> wrap_with_cfg ind cfg (Send (per, view_name ind cfg ch)) @@ view_procs ind cfg (Procs ps)
 | Match (p, mcs) ->
     (match mcs with
@@ -165,8 +165,7 @@ and view_proc_pat ind cfg = function
 | BoolPat -> String.make ind ' ' ^ "Bool"
 | IntPat -> String.make ind ' ' ^ "Int"
 | StrPat -> String.make ind ' ' ^ "String"
-| Wildcard -> String.make ind ' ' ^ "_"
-| PatVar x -> String.make ind ' ' ^ x
+| PWildcard -> String.make ind ' ' ^ "_"
 | Proc p -> String.make ind ' ' ^ view_proc ind ~cfg p
 | BundlePat (true,  true,  p) -> view_proc_pat ind cfg p
 | BundlePat (true,  false, p) -> wrap_with_cfg ind cfg BundlePlus  @@ view_proc_pat ind cfg p
@@ -189,7 +188,7 @@ and view_names ind cfg (Names ns) = String.concat ", " @@ List.map (view_name in
 
 and view_name_pat ind cfg = function
 | QuotePat pat -> "@" ^ (view_proc_pat ind cfg pat)
-| NPatVar x -> x
+| NWilcdcard -> "_"
 
 and view_name_pats ind cfg (NPats nps) = String.concat ", " @@ List.map (view_name_pat ind cfg) nps
 
@@ -214,31 +213,39 @@ and view_bind_pats ind cfg (BindPats (bk, l)) = match l with
 | (np, n) :: tl ->
     view_bind_pat ind cfg np n bk ^ String.concat "; " @@ List.map (fun (x, y) -> view_bind_pat ind cfg x y bk) tl
 
-and wrap_with_cfg _ind cfg =
+and wrap_with_cfg ind cfg ?(new_line = false) =
 function
 | Braces ->
     fun s ->
-      if cfg.new_line_brace then sprintf "{\n %s \n}" s
-      else if cfg.fluffy_brace then sprintf "{ %s }" s
-      else sprintf "{%s}" s
+      let sp = if cfg.new_line_brace then "\n"
+        else if cfg.fluffy_brace then " "
+        else sprintf ""
+      in
+      sprintf "{%s%s%s}" sp s sp
 | Parens ->
     fun s ->
-      if cfg.fluffy_paren then sprintf "( %s )" s
-      else sprintf "(%s)" s
+      let sp = if cfg.fluffy_paren then " " else "" in
+      sprintf "(%s%s%s)" sp s sp
 | For bs ->
     fun s ->
-      sprintf (
-        if cfg.fluffy_for then "for (%s) {%s}"
-        else "for(%s){%s}")
-        bs
-        s
+      let sp = if cfg.fluffy_for then " " else "" in
+      let sp1 = if cfg.fluffy_brace then " " else "" in
+      sprintf "for%s(%s)%s{%s%s%s}" 
+        sp bs sp sp1 s sp1
 | Send (per, ch) ->
     fun s ->
-      sprintf (
-        if cfg.fluffy_paren then "%s%s( %s )"
-        else "%s%s(%s)")
+      let sp = if cfg.fluffy_paren then " " else "" in
+      sprintf "%s%s(%s%s%s)"
         ch
         (if per then "!!" else "!")
+        sp
+        s
+        sp
+| WNew nl ->
+    fun s ->
+      sprintf "new %s%sin {\n%s\n}"
+        nl
+        (if new_line then "\n" else " ")
         s
 | _ -> fun _ -> ""
 
